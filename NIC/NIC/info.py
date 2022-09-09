8b320c0675e323af783e0e91838d103d4e4e5b98
KalyanRam <kalyan.2405@gmail.com>
2022-09-09 15:42:12 +0530

    Update info.py

--- NIC/NIC/info.py
+++ NIC/NIC/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kvkram2405@gmail.com'
-EMAIL_HOST_PASSWORD = 'fogbwpgmpxksboej'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
